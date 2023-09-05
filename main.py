6985d840356fbe2f2f2ca175b9cf1661258d1bff
Replit user <>
2023-09-05 02:09:34 +0000

    Initial commmit

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
-from flask import Flask, jsonify, render_template, request
-from database import load_jobs_from_db, load_job_from_db 
+from flask import Flask, render_template
+# request
+# from database import load_jobs_from_db, load_job_from_db 
 # add_application_to_db
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_jovian():
-  jobs = load_jobs_from_db()
-  return render_template('index.html', 
-                         jobs=jobs)
-@app.route("/job/<id>")
-def show_jobs(id):
-    job = load_job_from_db(id)
-    if job is None:
-        return jsonify({})
-    else:
-        return render_template("job_pages.html",jobs=job)
+  # jobs = load_jobs_from_db()
+  return render_template('index.html') 
+                         # jobs=jobs)
+# @app.route("/job/<id>")
+# def show_jobs(id):
+#     job = load_job_from_db(id)
+#     if job is None:
+#         return jsonify({})
+#     else:
+#         return render_template("job_pages.html",jobs=job)
 
-@app.route("/job/<id>/apply", methods=['post'])
-def apply_jobs(id):
-  data = request.form
-  job = load_job_from_db(id)
-  # add_application_to_db(id, data)
-  return render_template('application_submitted.html', 
-                         application=data,
-                         jobs=job)
+# @app.route("/job/<id>/apply", methods=['post'])
+# def apply_jobs(id):
+#   data = request.form
+#   job = load_job_from_db(id)
+#   # add_application_to_db(id, data)
+#   return render_template('application_submitted.html', 
+#                          application=data,
+#                          jobs=job)
 @app.route("/about")
 def about():
   return render_template("about.html")
